1ea5ff40f49a3d93268cb7f8c8d8cd08c767a4de
Ryosuke Izumi <ryosuke.izumi62@gmail.com>
2021-10-12 11:16:09 +0900

    add msw

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,10 @@
 import 'tailwindcss/tailwind.css';
 import type { AppProps } from 'next/app';
 
+if (process.env.NODE_ENV === 'development') {
+  require('../mocks');
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
