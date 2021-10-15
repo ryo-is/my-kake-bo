30ae28ed0d6d25fe81648c56ba0139aaa0ff691b
Ryosuke Izumi <ryosuke.izumi62@gmail.com>
2021-10-15 16:15:00 +0900

    LayoutとFontFamily追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,17 @@
 // import '../styles/globals.css';
 import 'tailwindcss/tailwind.css';
 import type { AppProps } from 'next/app';
+import { Layout } from '@templates/Layout';
 
 if (process.env.NODE_ENV === 'development') {
   require('../mocks');
 }
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 export default MyApp;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        common: [
+          "'Avenir Next','Noto Sans JP', -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji",
+        ],
+      },
+    },
   },
   variants: {
     extend: {},
