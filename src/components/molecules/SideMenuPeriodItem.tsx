e778313a8c5981c5347e5d6c1eeba7a1ff8ad700
Ryosuke Izumi <ryosuke.izumi62@gmail.com>
2022-02-02 16:24:31 +0900

    一旦期間を決め打ちにした

--- src/components/molecules/SideMenuPeriodItem.tsx
+++ src/components/molecules/SideMenuPeriodItem.tsx
@@ -1,6 +1,6 @@
 import { Period } from '@recoil/periodState';
-import { IconButton } from '@atoms/IconButton';
-import { TrashIcon } from '@heroicons/react/outline';
+// import { IconButton } from '@atoms/IconButton';
+// import { TrashIcon } from '@heroicons/react/outline';
 
 interface SideMenuPeriodItemProps {
   period: Period;
@@ -9,19 +9,19 @@ interface SideMenuPeriodItemProps {
 export const SideMenuPeriodItem = ({ period }: SideMenuPeriodItemProps) => {
   const { name, startDate, endDate } = period;
 
-  const handleDeleteClick = () => {};
+  // const handleDeleteClick = () => {};
 
   return (
-    <div className="pl-5 py-2 flex items-center hover:bg-gray-700 cursor-pointer">
+    <div className="pl-5 py-3 flex items-center hover:bg-gray-700 cursor-pointer">
       <div>{name}</div>
       <div className="text-xs px-1 text-gray-400">{`${startDate}-${endDate}`}</div>
-      <IconButton
+      {/* <IconButton
         handleClick={handleDeleteClick}
         addClass="text-red-700 hover:bg-gray-500"
         tipText="削除"
       >
         <TrashIcon className="w-6 h-6" />
-      </IconButton>
+      </IconButton> */}
     </div>
   );
 };
--- src/hooks/usePeriods.ts
+++ src/hooks/usePeriods.ts
@@ -11,9 +11,83 @@ export const usePeriods = (): IUsePeriods => {
 
   const getPeriods = async () => {
     try {
-      const { periods } = await apiHelper.get<{ periods: Period[] }>({
-        path: 'api/periods',
-      });
+      // const { periods } = await apiHelper.get<{ periods: Period[] }>({
+      //   path: 'api/periods',
+      // });
+      const periods: Period[] = [
+        {
+          docID: '1',
+          name: '1月',
+          startDate: '2021-12-24',
+          endDate: '2022-01-24',
+        },
+        {
+          docID: '2',
+          name: '2月',
+          startDate: '2022-01-25',
+          endDate: '2022-02-24',
+        },
+        {
+          docID: '3',
+          name: '3月',
+          startDate: '2022-02-25',
+          endDate: '2022-03-24',
+        },
+        {
+          docID: '4',
+          name: '4月',
+          startDate: '2022-03-25',
+          endDate: '2022-04-24',
+        },
+        {
+          docID: '5',
+          name: '5月',
+          startDate: '2022-04-25',
+          endDate: '2022-05-24',
+        },
+        {
+          docID: '6',
+          name: '6月',
+          startDate: '2022-05-25',
+          endDate: '2022-06-23',
+        },
+        {
+          docID: '7',
+          name: '7月',
+          startDate: '2022-06-24',
+          endDate: '2022-07-24',
+        },
+        {
+          docID: '8',
+          name: '8月',
+          startDate: '2022-07-25',
+          endDate: '2022-08-24',
+        },
+        {
+          docID: '9',
+          name: '9月',
+          startDate: '2022-08-25',
+          endDate: '2022-09-22',
+        },
+        {
+          docID: '10',
+          name: '10月',
+          startDate: '2022-09-24',
+          endDate: '2022-10-24',
+        },
+        {
+          docID: '11',
+          name: '11月',
+          startDate: '2022-10-25',
+          endDate: '2022-11-24',
+        },
+        {
+          docID: '12',
+          name: '12月',
+          startDate: '2022-11-25',
+          endDate: '2022-12-23',
+        },
+      ];
       setState({
         periods,
       });
